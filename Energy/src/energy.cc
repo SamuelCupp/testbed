6bab08d636d433e1297bd4f9529e22caf4b828db
Samuel Cupp <scupp1@my.apsu.edu>
2017-05-22 14:30:10 -0500

    This update includes correct information for all ccl files.

--- Energy/src/energy.cc
+++ Energy/src/energy.cc
@@ -2,29 +2,34 @@
 #include "cctk_Arguments.h"
 #include "cctk_Parameters.h"
 
-void WaveMoL_Energy(CCTK_ARGUMENTS)
+void presync_energy(CCTK_ARGUMENTS)
 {
 
   DECLARE_CCTK_ARGUMENTS;
+  DECLARE_CCTK_PARAMETERS;
       
-  int i,j,k;
-  int index;
-
-  for (k=0; k<cctk_lsh[2]; k++)
+  const int imin0=cctk_nghostzones[0];
+  const int imin1=cctk_nghostzones[1];
+  const int imin2=cctk_nghostzones[2];
+  const int imax0=cctk_lsh[0] - cctk_nghostzones[0];
+  const int imax1=cctk_lsh[1] - cctk_nghostzones[1];
+  const int imax2=cctk_lsh[2] - cctk_nghostzones[2];
+  const int zero = CCTK_GFINDEX3D(cctkGH,0,0,0);
+  const int di = (cctk_lsh[0]==1) ? 0:CCTK_GFINDEX3D(cctkGH,1,0,0) - zero;
+  const int dj = (cctk_lsh[1]==1) ? 0:CCTK_GFINDEX3D(cctkGH,0,1,0) - zero;
+  const int dk = (cctk_lsh[2]==1) ? 0:CCTK_GFINDEX3D(cctkGH,0,0,1) - zero;
+  #pragma omp parallel
+  CCTK_LOOP3(calc_energy,i,j,k,
+    imin0,imin1,imin2,imax0,imax1,imax2,
+    cctk_ash[0],cctk_ash[1],cctk_ash[2])
   {
-    for (j=0; j<cctk_lsh[1]; j++)
-    {
-      for (i=0; i<cctk_lsh[0]; i++)
-      {
-        index = CCTK_GFINDEX3D(cctkGH,i,j,k);
-        energy[index] = phit[index]*phit[index] +
-          phix[index]*phix[index] +
-          phiy[index]*phiy[index] +
-          phiz[index]*phiz[index];
-      }
-    }
+    int cc = CCTK_GFINDEX3D(cctkGH,i,j,k);
+    double phix = (phi[cc+di]-phi[cc-di])/(2.0*CCTK_DELTA_SPACE(0));
+    double phiy = (phi[cc+dj]-phi[cc-dj])/(2.0*CCTK_DELTA_SPACE(1));
+    double phiz = (phi[cc+dk]-phi[cc-dk])/(2.0*CCTK_DELTA_SPACE(2));
+    energy[cc] = psi[cc]*psi[cc] + phix*phix +
+      phiy*phiy + phiz*phiz;
   }
+  CCTK_ENDLOOP3(calc_energy);
   
 }
-
-          
